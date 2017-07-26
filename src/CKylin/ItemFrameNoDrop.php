8f634fa3df389b2223cd7f0638a0862e71581267
CKylinMC <Cansll@users.noreply.github.com>
2017-07-26 12:00:14 +0800

    ItemFrames is now breakable

--- src/CKylin/ItemFrameNoDrop.php
+++ src/CKylin/ItemFrameNoDrop.php
@@ -8,6 +8,9 @@ use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\Server;
+use pocketmine\item\ItemBlock;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
 use pocketmine\event\block\ItemFrameDropItemEvent;
 
 class ItemFrameNoDrop extends PluginBase implements Listener
@@ -18,6 +21,14 @@ class ItemFrameNoDrop extends PluginBase implements Listener
 	}
 
     public function onDrop(ItemFrameDropItemEvent $e){
+        $itemframe = $e->getItemFrame()->getBlock();
         $e->setCancelled(true);
+        $level = $itemframe->level;
+        $x = $itemframe->x;
+        $y = $itemframe->y;
+        $z = $itemframe->z;
+        $pos = new Vector3($x, $y, $z);
+        $airBlock = Item::fromString('0')->getBlock();
+        $level->setBlock($pos, $airBlock);
     }
 }
